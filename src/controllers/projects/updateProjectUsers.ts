53504f8555003f039a82ec99a1035fb45df50753
Margus Liinev <margus.liinev@gmail.com>
2023-07-31 21:17:29 +0300

    resetting ticket status and assigned user when the current assigned user gets removed from project

--- src/controllers/projects/updateProjectUsers.ts
+++ src/controllers/projects/updateProjectUsers.ts
@@ -1,8 +1,8 @@
-import { and, eq, ne } from 'drizzle-orm';
+import { and, eq, inArray, ne } from 'drizzle-orm';
 import { Response } from 'express';
 
 import { db } from '../../db/index';
-import { projects, projects_users, User } from '../../db/schema';
+import { projects, projects_users, tickets, User } from '../../db/schema';
 import { AuthenticatedRequest, UnauthenticatedError } from '../../utils';
 
 export const updateProjectUsers = async (req: AuthenticatedRequest, res: Response) => {
@@ -19,6 +19,23 @@ export const updateProjectUsers = async (req: AuthenticatedRequest, res: Respons
         .from(projects)
         .where(eq(projects.project_id, Number(project_id)));
     const manager_id = manager_id_query[0].manager_id;
+
+    const current_users = await db
+        .select({ user_id: projects_users.user_id })
+        .from(projects_users)
+        .where(and(eq(projects_users.project_id, Number(project_id)), ne(projects_users.user_id, manager_id)));
+
+    const removed_users = current_users.filter((user) => !updated_users.includes(user.user_id as number)).map((user) => user.user_id as number);
+
+    if (removed_users.length >= 1) {
+        await db
+            .update(tickets)
+            .set({ assigned_user_id: null, status: 'unassigned' })
+            .where(and(eq(tickets.project_id, Number(project_id)), inArray(tickets.assigned_user_id, removed_users)))
+            .catch(() => {
+                throw new Error('Failed to reset tickets');
+            });
+    }
 
     await db
         .delete(projects_users)
